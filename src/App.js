b05ffe7f3b78b26f6450b66b03a1d561345266fb
Abhishek Tekade <abhishektekade45@gmail.com>
2020-12-15 15:09:30 +0530

    Todo App Completed But not Stisfied with styles

--- src/App.js
+++ src/App.js
@@ -1,58 +1,87 @@
 import React, { Component } from 'react';
+import NewTodoForm from './NewTodoForm';
+import TodoList from './TodoList.js';
+import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      message : 'Todo App',
+      message: <h1 id="title">This is a Todo App!!</h1>,
       newTodo: '',
       todos: []
     };
   }
-  newTodoChanged(event){
+
+  newTodoChanged(event) {
     this.setState({
       newTodo: event.target.value
-     })
+    });
   }
 
-  formSubmitted(event){
+  formSubmitted(event) {
     event.preventDefault();
+
     this.setState({
       newTodo: '',
-      todos: [...this.state.todos,  {
+      todos: [...this.state.todos, {
         title: this.state.newTodo,
         done: false
       }]
-     });
-  }
-  toggleTodoDone(event){
-    console.log(event.target.checked)
-    const todos = [...this.state.todos]
-    this.setState({
-      newTodo: '',
-      todos: [...this.state.todos,  {
-        title: this.state.newTodo,
-        done: false
-      }]
-     });
+    });
   }
 
+  toggleTodoDone(event, index) {
+    const todos = [...this.state.todos];
+    todos[index] = {
+      ...todos[index],
+      done: event.target.checked
+    };
+    this.setState({
+      todos
+    });
+  }
+
+  removeTodo(index) {
+    const todos = [...this.state.todos]; // copy the array
+    todos.splice(index, 1);
+
+    this.setState({
+      todos
+    });
+  }
+
+  allDone() {
+    const todos = this.state.todos.map(todo => {
+      return {
+        title: todo.title, // can also do ...todo
+        done: true
+      };
+    });
+
+    this.setState({
+      todos
+    });
+  }
 
   render() {
     return (
       <div className="App">
         <h3>{this.state.message}</h3>
-        <form onSubmit={(event) => this.formSubmitted(event)}>  
-        <label  htmlFor="newTodo">New Todo</label>
-        <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" value={this.state.newTodo}/>
-        <button type="submit">Add Todo</button>
-        </form>
-        <ul>{this.state.todos.map((todo, index)=>{
-          return (<li key={todo.title} >
-            <input onChange={(event) => this.toggleTodoDone(event, index)} type="checkbox"/>{todo.title}
-            </li>)
-        })}</ul>
+        <NewTodoForm
+            newTodo={this.state.newTodo}
+            formSubmitted={this.formSubmitted.bind(this)}
+            newTodoChanged={this.newTodoChanged.bind(this)} />
+        <button onClick={() => this.allDone()}>All Done</button>
+        <TodoList
+          todos={this.state.todos}
+          toggleTodoDone={this.toggleTodoDone.bind(this)}
+          removeTodo={this.removeTodo.bind(this)}/>
+          <div>
+            <p id="react">Made with React</p>
+          </div>
       </div>
+      
     );
   }
 }
